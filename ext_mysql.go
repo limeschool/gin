91550760feb772cac49578a75a89a3350600f551
lime <8695643+limeschool@user.noreply.gitee.com>
2022-11-12 14:31:18 +0800

    [feature] 修改mysql耗时计算

--- ext_mysql.go
+++ ext_mysql.go
@@ -125,5 +125,5 @@ type DeleteModel struct {
 	ID        int64          `gorm:"primary_key" json:"id"`
 	CreatedAt int64          `json:"created_at,omitempty"`
 	UpdatedAt int64          `json:"updated_at,omitempty"`
-	DeletedAt gorm.DeletedAt `json:"deleted_at,omitempty" gorm:"index"`
+	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`
 }
